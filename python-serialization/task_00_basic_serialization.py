dd21506d3c0e227bb35aba3c2646170a2480a49c
Hikarikedo <keriandouidi01@gmail.com>
2025-02-12 23:19:05 +0100

    adding task 1 of serialization project

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""0. Basic Serialization"""
+
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """function that serialize and save data"""
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
+
+
+def load_and_deserialize(filename):
+    """function that load and deserialize data"""
+    with open(filename, 'r') as json_file:
+        data = json.load(json_file)
+    return data
